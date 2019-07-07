f6e3706d6eca1c8f17accd82d8d5f100ef7cc74d
Henry Campos <henry.campos@galileo.edu>
2019-07-07 14:50:04 -0600

    Gifs added to staff and expanded staff section added

--- gulpfile.js
+++ gulpfile.js
@@ -10,33 +10,16 @@ var autoprefixer = require('gulp-autoprefixer'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     rename = require('gulp-rename'),
-    uglify = require('gulp-uglify'),
-    images = require('gulp-imagemin'),
-    browserSync = require('browser-sync').create();
+    uglify = require('gulp-uglify');
 
 
 // paths
-var styleSrc = 'source/sass/**/*.scss',
-    styleDest = 'build/assets/css/',
-    htmlSrc = 'source/',
-    htmlDest = 'build/',
-    vendorSrc = 'source/js/vendors/',
-    actionSrc = 'source/js/actions/',
-    vendorDest = 'build/assets/js/',
-    scriptSrc = 'source/js/*.js',
-    backEndSrc = 'source/php/**/*.php',
-    scriptDest = 'build/assets/js/';
+var styleSrc = 'source/sass/**/*.scss';
 
 
-
-// --------------------------------------------
-// Stand Alone Tasks
-// --------------------------------------------
-
-
-// Compiles all SASS files
-gulp.task('sass', function() {
-    gulp.src('source/sass/**/*.scss')
+// Compiles all SCSS files
+gulp.task('sass', function(done) {
+    gulp.src(styleSrc)
         .pipe(plumber())
         .pipe(sass({
             style: 'compressed'
@@ -47,61 +30,15 @@ gulp.task('sass', function() {
           }))
 
         .pipe(gulp.dest('build/assets/css'));
-});
-
-gulp.task('php', function(){
-    gulp.src('source/php/**/*.php')
-        .pipe(gulp.dest('build/assets/php'));
-});
-
-gulp.task('images', function() {
-    gulp.src('source/img/*')
-        .pipe(images())
-        .pipe(gulp.dest('build/assets/img'));
-});
-
-// Uglify js files
-gulp.task('scripts', function() {
-    gulp.src('source/js/*.js')
-        .pipe(plumber())
-        .pipe(uglify())
-        .pipe(gulp.dest('build/assets/js'));
-});
-
-//Concat and Compress Vendor .js files
-gulp.task('vendors', function() {
-    gulp.src(
-            [
-                'source/js/vendors/jquery.min.js',
-                'source/js/vendors/*.js'
-            ])
-        .pipe(plumber())
-        .pipe(concat('vendors.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('build/assets/js'));
-});
-
-//Concat and Compress Actions .js files
-gulp.task('actions', function() {
-    gulp.src(
-            [
-                'source/js/actions/*.js'
-            ])
-        .pipe(plumber())
-        .pipe(concat('actions.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('build/assets/js'));
+    done();
 });
 
 // Watch for changes
 gulp.task('watch', function(){
-    gulp.watch(styleSrc,['sass']);
-    gulp.watch(scriptSrc,['scripts']);
-    gulp.watch(vendorSrc,['vendors']);
-    gulp.watch(actionSrc,['actions']);
-    gulp.watch(backEndSrc,['php']);
+    gulp.watch(styleSrc, gulp.series('sass'));
 });
 
 
-// use default task to launch Browsersync and watch JS files
-gulp.task('default', [ 'sass', 'scripts', 'vendors', 'actions', 'php', 'watch'], function () {});
+
+// gulp.task('default', [ 'scss', 'watch'], function () {});  //gulp v3
+gulp.task('default', gulp.parallel('sass', 'watch'));
